7b5b7c85a32c4ef21296d98b08c738931e142bb2
VuDiep <Vudiep621@gmail.com>
2023-12-17 00:11:11 +0700

    add layout default

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,13 @@
-function Header() {
-    return ( <h2> header </h2> );
+import classNames from "classnames/bind";
+import styles from "./SideBar.module.scss";
+
+const cx = classNames.bind(styles);
+function SideBar() {
+    return ( 
+        <aside className={cx('wrapper')}>
+            <h2>SideBar</h2>
+        </aside>
+     );
 }
 
-export default Header;
+export default SideBar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,18 @@
+import classNames from "classnames/bind";
 import SideBar from "./SideBar";
 import Header from "@/components/Layout/components/Header";
+import styles from "./DefaultLayout.module.scss"
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({children}) {
     return (
-         <div>
+         <div className={cx('wrapper')}>
             <Header/>
-            <div className="container">{children}</div>
-            <SideBar/>
+            <div className={cx('container')}>
+                <SideBar/>
+                <div className= {cx('content')}>{children}</div>
+            </div>
          </div> 
     );
 }
